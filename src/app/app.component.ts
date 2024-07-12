3e93f6f074b8ae0d93b33abfe176c07653cef3bd
eduardo <eduardocardoso1039@gmail.com>
2024-07-12 20:36:19 -0300

    Refactor | Core - Corrigido nome do projero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from "./shared/toolbar/toolbar.component";
 
 @Component({
   selector: 'flg-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'flght-app';
+  title = 'flight-app';
 }
