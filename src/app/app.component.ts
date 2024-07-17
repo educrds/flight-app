8aa8350578ad1fd7cea507eada79d8e83d9cd78c
eduardo <eduardocardoso1039@gmail.com>
2024-07-17 20:03:54 -0300

    Feature | App - Adicionado internacionalizaçao do primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,66 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { ToolbarComponent } from "./shared/toolbar/toolbar.component";
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BannerComponent } from "./shared/components/banner/banner.component";
+import { SearchFormComponent } from "./features/search-form/search-form.component";
+import { ToolbarComponent } from "./shared/components/toolbar/toolbar.component";
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
-  selector: 'flg-root',
+  selector: "flg-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ToolbarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [CommonModule, ToolbarComponent, BannerComponent, SearchFormComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  title = 'flight-app';
+export class AppComponent implements OnInit {
+  title = "flight-app";
+
+  constructor(private _config: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this._config.setTranslation({
+      accept: 'Sim',
+      reject: 'Não',
+      dayNames: [
+        'Domingo',
+        'Segunda',
+        'Terça',
+        'Quarta',
+        'Quinta',
+        'Sexta',
+        'Sábado',
+      ],
+      dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+      dayNamesMin: ['Do', 'Se', 'Te', 'Qu', 'Qu', 'Se', 'Sa'],
+      monthNames: [
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro',
+      ],
+      monthNamesShort: [
+        'Jan',
+        'Fev',
+        'Mar',
+        'Abr',
+        'Mai',
+        'Jun',
+        'Jul',
+        'Ago',
+        'Set',
+        'Out',
+        'Nov',
+        'Dez',
+      ],
+      dateFormat: 'dd/mm/yy'
+    });
+  }
 }
