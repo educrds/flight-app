5d2dc745efb98928160a4ce6e1938222b48ce92a
eduardo <eduardocardoso1039@gmail.com>
2024-07-21 20:33:14 -0300

    Refactor | SearchForm - Refatorado código

--- src/app/features/search-form/search-form.component.ts
+++ src/app/features/search-form/search-form.component.ts
@@ -1,22 +1,23 @@
 import { Component, inject } from "@angular/core";
-import { StatesAutocompleteComponent } from "../../shared/components/states-autocomplete/states-autocomplete.component";
+import { AirportsAutocompleteComponent } from "../../shared/components/airports-autocomplete/airports-autocomplete.component";
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 import { DatepickerComponent } from "../../shared/datepicker/datepicker.component";
-import { TabViewModule } from 'primeng/tabview';
-import { ButtonModule } from 'primeng/button';
-import { InputNumberModule } from 'primeng/inputnumber';
+import { TabViewModule } from "primeng/tabview";
+import { ButtonModule } from "primeng/button";
+import { InputNumberModule } from "primeng/inputnumber";
+import { FlightsService } from "../../core/services/flights.service";
 
 @Component({
   selector: "flg-search-form",
   standalone: true,
   imports: [
-    StatesAutocompleteComponent,
+    AirportsAutocompleteComponent,
     ReactiveFormsModule,
     DatepickerComponent,
     FormsModule,
     TabViewModule,
     ButtonModule,
-    InputNumberModule
+    InputNumberModule,
   ],
   templateUrl: "./search-form.component.html",
   styleUrl: "./search-form.component.scss",
@@ -25,6 +26,7 @@ export class SearchFormComponent {
   protected searchForm!: FormGroup;
 
   #_fb = inject(FormBuilder);
+  #_flightsService = inject(FlightsService);
 
   constructor() {
     this.searchForm = this.#_fb.group({
@@ -32,17 +34,11 @@ export class SearchFormComponent {
       destino: [null, Validators.required],
       adultos: [1, Validators.required],
       criancas: [0, Validators.required],
-      dateRange: [{ start: new Date(), end: new Date() }],
-      dates: [
-        {
-          partida: [null],
-          retorno: [null],
-        },
-      ],
+      dateRange: [{ partida: new Date(), retorno: new Date() }]
     });
   }
 
-protected changeControls(): void {
+  protected changeControls(): void {
     const origemValue = this.searchForm.get("origem")?.value;
     const destinoValue = this.searchForm.get("destino")?.value;
 
@@ -54,5 +50,8 @@ protected changeControls(): void {
 
   protected searchFlights() {
     console.log(this.searchForm.getRawValue());
+    this.#_flightsService.getFlights(this.searchForm.getRawValue()).subscribe({
+      next: res => console.log(res),
+    });
   }
 }
