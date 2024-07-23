71bdb0c561d067e0bd5d7138e39858eb2ae1ff72
eduardo <eduardocardoso1039@gmail.com>
2024-07-22 21:05:53 -0300

    Refactor | AirportService - Adicionado nova tipagem e nomenclatura do metodo

--- src/app/shared/services/airport.service.ts
+++ src/app/shared/services/airport.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import { Observable, shareReplay } from "rxjs";
 import { environment } from "../../../environments/environment";
-import { AirportResponse } from "../models/Airport";
+import { ApiReponse } from "../models/ApiResponse";
+import { Airport } from "../models/Airport";
 
 @Injectable({
   providedIn: "root",
 })
 export class AirportService {
-  private airportListCache$?: Observable<AirportResponse>;
+  private airportListCache$?: Observable<ApiReponse<Airport[]>>;
 
   #envinronment = environment;
   #_http = inject(HttpClient);
@@ -17,14 +18,14 @@ export class AirportService {
     headers: this.#envinronment.apiHeaders
   };
 
-  public getStatesList$(): Observable<AirportResponse> {
+  public getAirportsList$(): Observable<ApiReponse<Airport[]>> {
     if (!this.airportListCache$) {
       this.airportListCache$ = this.requestAirportsList().pipe(shareReplay(1));
     }
     return this.airportListCache$;
   }
 
-  private requestAirportsList(): Observable<AirportResponse> {
-    return this.#_http.get<AirportResponse>(`${this.#envinronment.flightApiUrl}/airports`, this.#headerParams);
+  private requestAirportsList(): Observable<ApiReponse<Airport[]>> {
+    return this.#_http.get<ApiReponse<Airport[]>>(`${this.#envinronment.flightApiUrl}/airports`, this.#headerParams);
   }
 }
