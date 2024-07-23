53bf57181261e2fb21540b6d87b15d1d2c92c105
eduardo <eduardocardoso1039@gmail.com>
2024-07-23 20:20:14 -0300

    Refactor | AirportService - Removido método obsoleto

--- src/app/shared/services/airport.service.ts
+++ src/app/shared/services/airport.service.ts
@@ -1,31 +1,18 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
-import { Observable, shareReplay } from "rxjs";
+import { Observable } from "rxjs";
 import { environment } from "../../../environments/environment";
-import { ApiReponse } from "../models/ApiResponse";
 import { Airport } from "../models/Airport";
 
 @Injectable({
   providedIn: "root",
 })
 export class AirportService {
-  private airportListCache$?: Observable<ApiReponse<Airport[]>>;
-
   #envinronment = environment;
   #_http = inject(HttpClient);
 
-  #headerParams = {
-    headers: this.#envinronment.apiHeaders
-  };
-
-  public getAirportsList$(): Observable<ApiReponse<Airport[]>> {
-    if (!this.airportListCache$) {
-      this.airportListCache$ = this.requestAirportsList().pipe(shareReplay(1));
-    }
-    return this.airportListCache$;
-  }
-
-  private requestAirportsList(): Observable<ApiReponse<Airport[]>> {
-    return this.#_http.get<ApiReponse<Airport[]>>(`${this.#envinronment.flightApiUrl}/airports`, this.#headerParams);
+  public getAirportsList$(query: string): Observable<Airport> {
+    const url = `${this.#envinronment.airportsApiUrl}/suggest/${encodeURIComponent(query)}`
+    return this.#_http.get<Airport>(url);
   }
 }
