b520e8d7e99a47ead6ef7cd2ce7d56ca73a5dcbd
eduardo <eduardocardoso1039@gmail.com>
2024-07-22 21:06:53 -0300

    Refactor | SearchForm - Refatorado código e uso da store de pesquisas

--- src/app/features/search-form/search-form.component.ts
+++ src/app/features/search-form/search-form.component.ts
@@ -6,6 +6,9 @@ import { TabViewModule } from "primeng/tabview";
 import { ButtonModule } from "primeng/button";
 import { InputNumberModule } from "primeng/inputnumber";
 import { FlightsService } from "../../core/services/flights.service";
+import { Flight } from "../../models/Flight";
+import { finalize } from "rxjs";
+import { FlightStateService } from "../../shared/services/flight-state.service";
 
 @Component({
   selector: "flg-search-form",
@@ -24,9 +27,11 @@ import { FlightsService } from "../../core/services/flights.service";
 })
 export class SearchFormComponent {
   protected searchForm!: FormGroup;
+  protected flights: Flight[] | undefined;
 
   #_fb = inject(FormBuilder);
   #_flightsService = inject(FlightsService);
+  #_flightStateService = inject(FlightStateService);
 
   constructor() {
     this.searchForm = this.#_fb.group({
@@ -34,24 +39,26 @@ export class SearchFormComponent {
       destino: [null, Validators.required],
       adultos: [1, Validators.required],
       criancas: [0, Validators.required],
-      dateRange: [{ partida: new Date(), retorno: new Date() }]
+      date: [null, Validators.required]
     });
   }
 
   protected changeControls(): void {
     const origemValue = this.searchForm.get("origem")?.value;
     const destinoValue = this.searchForm.get("destino")?.value;
-
-    this.searchForm.get("origem")?.setValue(destinoValue, { emitEvent: false });
-    this.searchForm.get("destino")?.setValue(origemValue, { emitEvent: false });
-
-    this.searchForm.updateValueAndValidity();
+  
+    if(origemValue && destinoValue){
+      this.searchForm.get("origem")?.setValue(destinoValue, { emitEvent: false });
+      this.searchForm.get("destino")?.setValue(origemValue, { emitEvent: false });
+    
+      this.searchForm.updateValueAndValidity();
+    }
   }
-
+  
   protected searchFlights() {
     console.log(this.searchForm.getRawValue());
     this.#_flightsService.getFlights(this.searchForm.getRawValue()).subscribe({
-      next: res => console.log(res),
+      next: res =>  this.#_flightStateService.storeFlightList(res.data),
     });
   }
 }
